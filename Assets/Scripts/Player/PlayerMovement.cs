a0a1d852c431bab6309055fffca0ffd591230814
NotBip <hamadmohamed0009@gmail.com>
2024-05-14 02:29:20 -0400

    raaa

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool isGrounded;
     private Vector3 playerVelocity; 
-    private float gravity = -9.8f; 
+    private float gravity = -19.6f; 
     private float jumpForce = 1f; 
     private float speedMultiplier = 1.5f; 
     private bool isCrouching = false;
